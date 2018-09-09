46818cc631d237740fcd888d82fcb8c0c2481538
siddhantpandey <siddhantpandey59@gmail.com>
2018-09-09 15:04:51 +0530

    09-Sept-2018

--- SkyTelSPA/src/app/app.module.ts
+++ SkyTelSPA/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Router } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MediaComponent } from './media/media.component';
@@ -8,6 +8,12 @@ import { RegisterComponent } from './register/register.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { CareersComponent } from './careers/careers.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { BusinessComponent } from './business/business.component';
+
+import { NotfoundComponent } from './notfound/notfound.component';
+
 
 @NgModule({
   declarations: [
@@ -17,10 +23,70 @@ import { CareersComponent } from './careers/careers.component';
     RegisterComponent,
     ContactUsComponent,
     AboutUsComponent,
-    CareersComponent
+    CareersComponent,
+    NavbarComponent,
+    HomeComponent,
+    BusinessComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path:'', component: HomeComponent
+      },
+      {
+        path:'aboutus', component: AboutUsComponent
+      },
+      {
+        path: 'contactus', component: ContactUsComponent
+      },
+      {
+        path: 'careers', component: CareersComponent 
+      },
+      {
+        path: 'login', component: LoginComponent
+      },
+      {
+        path: 'register', component: RegisterComponent
+      },
+      {
+        path: 'business',component: BusinessComponent
+      }
+      ,
+      {
+        path: 'media',component: MediaComponent
+      },
+      {
+        path: '**',component: NotfoundComponent
+      }
+    ])
+   /* RouterModule.forChild([
+      {
+        path:'home', component: HomeComponent
+      },
+      {
+        path:'aboutus', component: AboutUsComponent
+      },
+      {
+        path: 'contactus', component: ContactUsComponent
+      },
+      {
+        path: 'careers', component: CareersComponent 
+      },
+      {
+        path: 'login', component: LoginComponent
+      },
+      {
+        path: 'register', component: RegisterComponent
+      },
+      {
+        path: 'business',component: BusinessComponent
+      },
+      {
+        path: '**',component: NotfoundComponent
+      }
+    ])*/
   ],
   providers: [],
   bootstrap: [AppComponent]
